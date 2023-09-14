b18653ef3a25a0d14b2237e1402910326f06cc04
ShelbyEliza <shelbyelizashipley@gmail.com>
2023-09-14 10:19:55 -0700

    Add Development features

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,30 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+	mode: "development",
 	entry: {
 		index: "./src/index.js",
 		print: "./src/print.js",
 	},
+	// shows which files errors originate from:
+	devtool: "inline-source-map",
+	devServer: {
+		static: "./dist",
+	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			title: "Output Management",
+			title: "Development",
 		}),
 	],
 	output: {
 		filename: "[name].bundle.js",
 		path: path.resolve(__dirname, "dist"),
+		// remove unused files after every build:
 		clean: true,
+		// needed for webpack-dev-middleware:
+		publicPath: "/",
 	},
-	mode: "development",
+	optimization: {
+		runtimeChunk: "single",
+	},
 };
